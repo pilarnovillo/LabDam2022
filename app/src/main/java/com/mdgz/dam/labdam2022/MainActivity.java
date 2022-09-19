cc30de91a83929a45895ed483d9b2aeb44ab266a
pilarnovillo <pilar.novillosaravia@gmail.com>
2022-09-19 16:20:07 +0200

    avances

--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -3,6 +3,11 @@ package com.mdgz.dam.labdam2022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialToolbar toolbar = findViewById(R.id.materialToolbar);
+        toolbar.setTitle("MiAlojamiento");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnuOpt1:
+//                Intent i1 = new Intent(this,Main3Activity.class);
+//                startActivity(i1);
+                return true;
+            case R.id.mnuOpt2:
+                return true;
+            default:
+                Toast.makeText(this, ". . . . ", Toast.LENGTH_LONG).show();
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
